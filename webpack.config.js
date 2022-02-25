e44a4e8dda6a2ddc8fcaec825adb4e13ab593db7
Luis Banegas <55450718+Shadow504@users.noreply.github.com>
2022-02-25 13:56:14 -0600

    implementacion cambios y PWA

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const WebpackPwaManifestPlugin = require('webpack-pwa-manifest')
+const WorkboxWebpackPlugin = require('workbox-webpack-plugin')
+const path = require('path')
 
 module.exports = {
   output: {
@@ -14,6 +17,35 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html'
+    }),
+    new WebpackPwaManifestPlugin({
+      name: 'Petgram - Tu app de fotos de mascotas',
+      short_name: 'Petgram',
+      description: 'Con petgram tendran tendras a tu disposicion fotos',
+      background_color: '#fff',
+      theme_color: '#b1a',
+      icons: [{
+        src: path.resolve('src/assets/icon.jpg'),
+        sizes: [96, 128, 192, 256, 384, 512]
+      }]
+    }),
+    new WorkboxWebpackPlugin.GenerateSW({
+      runtimeCaching: [
+        {
+          urlPattern: new RegExp('https://(res.cloudinary.com | images.unsplash.com)'),
+          handler: 'CacheFirst',
+          options: {
+            cacheName: 'images'
+          }
+        },
+        {
+          urlPattern: new RegExp('https://petgram-server-luis-b.vercel.app/'),
+          handler: 'NetworkFirst',
+          options: {
+            cacheName: 'api'
+          }
+        }
+      ]
     })
   ],
   module: {
